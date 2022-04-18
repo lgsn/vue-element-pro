c11f72373eab89cf6e76d0c6791ab2f46029467e
开关 <gltwork@163.com>
2022-04-18 11:34:26 +0800

    fix

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,7 +1,8 @@
 module.exports = {
   // 站点配置
   lang: 'zh-CN',
-  title: '/vue-element-pro/',
+  title: 'vue-element-pro',
+  base: '/vue-element-pro/',
   description: '管理后台前端解决方案，基于Vue@2.6.11 和Element-UI',
   // 主题和它的配置
   theme: '@vuepress/theme-default',
